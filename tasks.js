024aac56dfa089fcf06132f8c78818061bbcfbfb
Fatme <fatme34saeed@gmail.com>
2024-12-17 11:05:21 +0200

    help command

--- tasks.js
+++ tasks.js
@@ -40,6 +40,9 @@ function onDataReceived(text) {
   else if(text === 'hello\n'){
     hello();
   }
+  else if(text === 'help\n'){
+    help();
+  }
   else{
     unknownCommand(text);
   }
@@ -76,6 +79,13 @@ function hello(){
 function quit(){
   console.log('Quitting now, goodbye!')
   process.exit();
+}
+
+/**
+ * Lists all the possible commands
+ */
+function help(){
+   console.log('All possible commands: \nhello\nquit\nexit\nunknownCommand\n');
 }
 
 // The following line starts the application
